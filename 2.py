45de81ee66f4075b63905e9ddb93050fade5ccdd
himhn7 <67187127+himhn7@users.noreply.github.com>
2020-06-20 15:01:31 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,25 @@
+def prime_nums(num):
+
+	primes = [2]
+
+	if num < 2:
+		return []
+	elif num == 2:
+		return primes
+
+	i = 3
+
+	while i <= num:
+		for j in range(3,i):
+			if i%j == 0:
+				i += 2
+				break
+		else:
+			primes.append(i)
+			i += 2
+
+	return primes
+
+if __name__ == '__main__':
+ 	a = prime_nums(100)
+ 	print(a)
